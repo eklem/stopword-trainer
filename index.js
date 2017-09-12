f0978d27b7471fe9c398d71f16ca99dd73bb0354
Espen Klem <espen.klem@gmail.com>
2017-09-12 15:25:55 +0200

    Counting number of docs processed, fixing issue #12

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ const ndjson = require('ndjson')
 const request = require('request')
 const _ = require('lodash')
 
-request('https://rawgit.com/fergiemcdowall/reuters-21578-json/master/data/fullFileStream/justTen.str')
+var docCount = 0
+request('https://rawgit.com/fergiemcdowall/reuters-21578-json/master/data/fullFileStream/full.str')
+//request('https://rawgit.com/fergiemcdowall/reuters-21578-json/master/data/fullFileStream/justTen.str')
   .pipe(ndjson.parse())
   .on('data', function(obj) {
-    // Swap this with something more robust later (lodash?)
+    docCount++
+    console.log('Document: #' + docCount)
     var text = ''
     for(var key in obj) {
-      var value = obj[key];
-      if (value.constructor === Array)  {
-      // Check if array
-        for (let arrayValue of value) {
-          //console.log(arrayValue)
-          text += (' ' + arrayValue)
-        }
-      } else {
-        //console.log(value)
-        text += (' ' + value)
-      }
+      var value = obj[key]
+      value = _.lowerCase(value)
+      text += (' ' + value)
     }
-    console.log(text)
-    // Do stuff with string:
-    // A: Get only words (regex)
-    // B: split up into array
-    // C: Add to array
-    // D: Check if in array from before and update count instead
+    var textArray = _.words(text, /[^, ]+/g)
+    //_.flatMapDeep(obj)
+    console.dir(textArray)
+    
+    
+    /*
+    ### Do stuff with string:
+    *A: Get only words & numbers(regex)
+    *B: split up into array
+     C: Add to array
+     D: Check if in array from before and update count instead
+    */
   })
