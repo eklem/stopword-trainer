c1749beca9588d2fac640f35c58f1d4cb54ef0c9
Espen Klem <espen.klem@gmail.com>
2017-09-13 13:45:19 +0200

    Counting inCorpus and writing to file, fixing #2

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const _ = require('lodash')
+const fs = require('fs')
 const ndjson = require('ndjson')
 const request = require('request')
-const _ = require('lodash')
 
-var docCount = 0
+let calculationArray = []
+let docCount = 0
+let stopwordArray = []
+
 request('https://rawgit.com/fergiemcdowall/reuters-21578-json/master/data/fullFileStream/full.str')
-//request('https://rawgit.com/fergiemcdowall/reuters-21578-json/master/data/fullFileStream/justTen.str')
   .pipe(ndjson.parse())
   .on('data', function(obj) {
     docCount++
@@ -16,15 +19,51 @@ request('https://rawgit.com/fergiemcdowall/reuters-21578-json/master/data/fullFi
       text += (' ' + value)
     }
     var textArray = _.words(text, /[^, ]+/g)
-    //_.flatMapDeep(obj)
-    console.dir(textArray)
-    
-    
-    /*
-    ### Do stuff with string:
-    *A: Get only words & numbers(regex)
-    *B: split up into array
-     C: Add to array
-     D: Check if in array from before and update count instead
-    */
+    //console.dir(textArray)
+    // loop throug array and call countWords for each word
+    for (let word of textArray) {
+      countWords(word)  
+    }
+    //console.log(calculationArray)
   })
+  .on('end', function () {
+    calculationArray =  _.sortBy(calculationArray, ['inCorpus']);
+    _.reverse(calculationArray)
+    console.log(calculationArray)
+    // Do calculation with calculationArray, sort the data and write stopwordArray to file
+    calculationJSON = JSON.stringify(calculationArray)
+    fs.writeFileSync('calculation.json', calculationJSON)
+    console.log('we have come to the end')
+  })
+
+/* Calculate per corups frequency */
+function countWords (word) {
+  if (typeof _.find(calculationArray, { 'word': word }) !== "undefined") {
+    //console.log('Hello found!')
+    let wordAtIndex = _.findIndex(calculationArray, {word: word});
+    //console.log('wordAtIndex: ' + wordAtIndex)
+    calculationArray[wordAtIndex].inCorpus = calculationArray[wordAtIndex].inCorpus + 1
+    //console.log(calculationArray[wordAtIndex])
+  }
+  if (typeof _.find(calculationArray, { 'word': word }) === "undefined") {
+    //console.log('Hello not found!')
+    var wordObject = {
+      word: word,
+      inCorpus: 1
+    }
+    calculationArray.push(wordObject)    
+  }
+}
+
+
+/* ### Do stuff with string:
+   *A: Get only words & numbers(regex)
+   *B: split up into array
+   *C: Add to calculation array
+   *D: Check if in array from before and update count instead
+    E: Do per-corpus calculation
+    G: Check if in doc from before and skip if yes
+    H: Do per-document calculation
+*/
+  
+  
