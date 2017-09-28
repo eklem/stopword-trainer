5db992045ea17c30c1219edef5dd748ecf8e4c83
Espen Klem <espen.klem@gmail.com>
2017-09-28 15:27:57 +0200

    A little closer with options object

--- index-cli.js
+++ index-cli.js
@@ -5,16 +5,25 @@ const ndjson = require('ndjson')
 const swt = require('./index.js')
 
 let dataStream = './node_modules/reuters-21578-json/data/fullFileStream/justTen.str'
-let docCount = 0
-let calculationArray = []
-let stopwordArray = []
-let obj = {}
+let opts = {
+  docCount: 0,
+  calculationArray: [],
+  stopwordArray: [],
+}
 
 fs.createReadStream(dataStream)
   .on('error', function(err) {
     console.log('Oops, an error: ' + err)
   })
-  .pipe(ndjson.parse())
-  .on('data', swt.termFrequency(obj, docCount, calculationArray))
-  .on('end', swt.documentFrequency())
+  .pipe(ndjson.parse(opts))
+  .on('data', function (obj) {
+    console.log(opts)
+    opts.docCount++
+    swt.termFrequency(obj, opts.docCount, opts.calculationArray)   
+  })
+  .on('end', function () {
+    console.log(calculationArray)
+  })
+
+  
 
--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const _ = require('lodash')
 
-// https://rawgit.com/fergiemcdowall/reuters-21578-json/master/data/fullFileStream/justTen.str
-// var config =  https://rawgit.com/eklem/stopword-trainer/master/config.json
-
-
-//  let config = {}
-//  let dataStream = './node_modules/reuters-21578-json/data/fullFileStream/justTen.str'
-
-
 /* Request data, count, do calculation, save files */
 
-var TermFrequency = function (obj, docCount, calculationArray) {
+let termFrequency = function (obj, docCount, calculationArray) {
   docCount++
   var text = ''
   for(var key in obj) {
@@ -20,23 +12,13 @@ var TermFrequency = function (obj, docCount, calculationArray) {
   }
   var textArray = _.words(text, /[^\,.!"#$%&()\[\]{}\/\\ 0-9\f\n\r\t]+/g)
   for (let word of textArray) {
-    countWords(word, docCount)  
+    countWords(word, docCount, calculationArray)
+    //console.log(word + ': ' + docCount)
   }
   console.log('Processing doc: #' + docCount)
-}
-var DocumentFrequency = function () {
-  calculateStopwords(calculationArray, docCount)
-  calculationArray = _.sortBy(calculationArray, ['stopWordiness']);
-  _.reverse(calculationArray)
-  stopwordArray = _.map(calculationArray, 'word');
-  calculationJSON = JSON.stringify(calculationArray)
-  stopwordJSON = JSON.stringify(stopwordArray)
-  fs.writeFileSync('stopwords-calculation.json', calculationJSON)
-  fs.writeFileSync('stopwords.json', stopwordJSON)
-  console.log((calculationArray.length + 1) + ' words in ' + docCount + ' documents processed')
-}
 
-function countWords (word, documentId) {
+
+function countWords (word, documentId, calculationArray) {
   if (typeof _.find(calculationArray, { 'word': word }) !== "undefined") {
     let wordAtIndex = _.findIndex(calculationArray, {word: word});
     calculationArray[wordAtIndex].inCorpus = calculationArray[wordAtIndex].inCorpus + 1
@@ -55,16 +37,13 @@ function countWords (word, documentId) {
     }
     calculationArray.push(wordObject)    
   }
+  console.log(calculationArray)
 }
+  return calculationArray
 
-function calculateStopwords (calculationArray, totalDocs) {
-  for (i = 0; i < calculationArray.length; ++i) {
-    calculationArray[i].stopWordiness = (calculationArray[i].inCorpus / totalDocs) * (1 / (Math.log(totalDocs/(calculationArray[i].inDocs - 1))))
-  }
 }
 
 // Export functions as swt:
 // var swt = require('stopword-trainer')
-module.exports.termFrequency = TermFrequency;
-module.exports.documentFrequency = DocumentFrequency;
+module.exports.termFrequency = termFrequency;
 
