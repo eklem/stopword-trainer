70ba28adfcc8db1bdf0a8d4209152e9be0beca2d
Espen Klem <espen.klem@gmail.com>
2017-09-28 07:22:53 +0200

    In between working versions. Transition to library + clientIn between working versions. Transition to library + client

--- index-cli.js
+++ index-cli.js
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+
+const fs = require('fs')
+const ndjson = require('ndjson')
+const swt = require('./index.js')
+
+let dataStream = './node_modules/reuters-21578-json/data/fullFileStream/justTen.str'
+let docCount = 0
+let calculationArray = []
+let stopwordArray = []
+let obj = {}
+
+fs.createReadStream(dataStream)
+  .on('error', function(err) {
+    console.log('Oops, an error: ' + err)
+  })
+  .pipe(ndjson.parse())
+  .on('data', swt.termFrequency(obj, docCount, calculationArray))
+  .on('end', swt.documentFrequency())
+
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const _ = require('lodash')
-const fs = require('fs')
-const ndjson = require('ndjson')
 
 // https://rawgit.com/fergiemcdowall/reuters-21578-json/master/data/fullFileStream/justTen.str
 // var config =  https://rawgit.com/eklem/stopword-trainer/master/config.json
@@ -12,70 +10,61 @@ const ndjson = require('ndjson')
 
 /* Request data, count, do calculation, save files */
 
-var StopwordTrainer = function (dataStream) {
-  let docCount = 0
-  let calculationArray = []
-  let stopwordArray = []
-
-  fs.createReadStream(dataStream)
-    .on('error', function(err) {
-      console.log(err)
-    })
-    .pipe(ndjson.parse())
-    .on('data', function(obj) {
-      docCount++
-      var text = ''
-      for(var key in obj) {
-        var value = obj[key]
-        value = _.lowerCase(value)
-        text += (' ' + value)
-      }
-      var textArray = _.words(text, /[^\,.!"#$%&()\[\]{}\/\\ 0-9\f\n\r\t]+/g)
-      for (let word of textArray) {
-        countWords(word, docCount)  
-      }
-      console.log('Processing doc: #' + docCount)
-    })
-    .on('end', function () {
-      calculateStopwords(calculationArray, docCount)
-      calculationArray = _.sortBy(calculationArray, ['stopWordiness']);
-      _.reverse(calculationArray)
-      stopwordArray = _.map(calculationArray, 'word');
-      calculationJSON = JSON.stringify(calculationArray)
-      stopwordJSON = JSON.stringify(stopwordArray)
-      fs.writeFileSync('stopwords-calculation.json', calculationJSON)
-      fs.writeFileSync('stopwords.json', stopwordJSON)
-      console.log((calculationArray.length + 1) + ' words in ' + docCount + ' documents processed')
-    })
-
-  function countWords (word, documentId) {
-    if (typeof _.find(calculationArray, { 'word': word }) !== "undefined") {
-      let wordAtIndex = _.findIndex(calculationArray, {word: word});
-      calculationArray[wordAtIndex].inCorpus = calculationArray[wordAtIndex].inCorpus + 1
-      // Do check on documentId > lastSpottedIn. If so, inDocs += 1 and lastSpottedIn = documentId
-      if (documentId > calculationArray[wordAtIndex].lastSpottedIn) {
-        calculationArray[wordAtIndex].inDocs = calculationArray[wordAtIndex].inDocs + 1
-        calculationArray[wordAtIndex].lastSpottedIn = documentId
-      }
-    } else if (typeof _.find(calculationArray, { 'word': word }) === "undefined") {
-      var wordObject = {
-        word: word,
-        inCorpus: 1,
-        inDocs: 1,
-        lastSpottedIn: documentId,
-        stopWordiness: 0
-      }
-      calculationArray.push(wordObject)    
-    }
+var TermFrequency = function (obj, docCount, calculationArray) {
+  docCount++
+  var text = ''
+  for(var key in obj) {
+    var value = obj[key]
+    value = _.lowerCase(value)
+    text += (' ' + value)
   }
+  var textArray = _.words(text, /[^\,.!"#$%&()\[\]{}\/\\ 0-9\f\n\r\t]+/g)
+  for (let word of textArray) {
+    countWords(word, docCount)  
+  }
+  console.log('Processing doc: #' + docCount)
+}
+var DocumentFrequency = function () {
+  calculateStopwords(calculationArray, docCount)
+  calculationArray = _.sortBy(calculationArray, ['stopWordiness']);
+  _.reverse(calculationArray)
+  stopwordArray = _.map(calculationArray, 'word');
+  calculationJSON = JSON.stringify(calculationArray)
+  stopwordJSON = JSON.stringify(stopwordArray)
+  fs.writeFileSync('stopwords-calculation.json', calculationJSON)
+  fs.writeFileSync('stopwords.json', stopwordJSON)
+  console.log((calculationArray.length + 1) + ' words in ' + docCount + ' documents processed')
+}
 
-  function calculateStopwords (calculationArray, totalDocs) {
-    for (i = 0; i < calculationArray.length; ++i) {
-      calculationArray[i].stopWordiness = (calculationArray[i].inCorpus / totalDocs) * (1 / (Math.log(totalDocs/(calculationArray[i].inDocs - 1))))
+function countWords (word, documentId) {
+  if (typeof _.find(calculationArray, { 'word': word }) !== "undefined") {
+    let wordAtIndex = _.findIndex(calculationArray, {word: word});
+    calculationArray[wordAtIndex].inCorpus = calculationArray[wordAtIndex].inCorpus + 1
+    // Do check on documentId > lastSpottedIn. If so, inDocs += 1 and lastSpottedIn = documentId
+    if (documentId > calculationArray[wordAtIndex].lastSpottedIn) {
+      calculationArray[wordAtIndex].inDocs = calculationArray[wordAtIndex].inDocs + 1
+      calculationArray[wordAtIndex].lastSpottedIn = documentId
     }
+  } else if (typeof _.find(calculationArray, { 'word': word }) === "undefined") {
+    var wordObject = {
+      word: word,
+      inCorpus: 1,
+      inDocs: 1,
+      lastSpottedIn: documentId,
+      stopWordiness: 0
+    }
+    calculationArray.push(wordObject)    
+  }
+}
+
+function calculateStopwords (calculationArray, totalDocs) {
+  for (i = 0; i < calculationArray.length; ++i) {
+    calculationArray[i].stopWordiness = (calculationArray[i].inCorpus / totalDocs) * (1 / (Math.log(totalDocs/(calculationArray[i].inDocs - 1))))
   }
 }
 
 // Export functions as swt:
 // var swt = require('stopword-trainer')
-module.exports.stopwordtrainer = StopwordTrainer;
+module.exports.termFrequency = TermFrequency;
+module.exports.documentFrequency = DocumentFrequency;
+
